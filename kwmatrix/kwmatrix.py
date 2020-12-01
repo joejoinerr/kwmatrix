a812d8283b741dca694439603bb0bf48e65f24ec
Joe Joiner <joejoinerr@gmail.com>
2020-12-01 09:44:32 +0000

    Comments and amends

--- kwmatrix/kwmatrix.py
+++ kwmatrix/kwmatrix.py
@@ -35,35 +35,35 @@ MODIFIERS = {
 def _subset_modifiers(seed: str, modifiers: dict, var_char=r'%') -> dict:
     lookup = var_char + r'([\w-]+)'
     seed_vars = re.findall(lookup, seed)
-    mods_subset = {k: modifiers[k] for k in seed_vars}
+
+    # Create a new subset dictionary where each modifier list is de-duplicated
+    mods_subset = {k: list(set(modifiers[k])) for k in seed_vars}
 
     return mods_subset
 
 
-def _combine(seed: str, modifiers: dict) -> list:
-    """Generate combinations for a single keyword."""
+def _combine(seed: str, modifiers: dict, var_char=r'%'):
     mods_product = [dict(zip(modifiers.keys(), p)) for p in product(*modifiers.values())]
-    combined = []
     for combination in mods_product:
-        new_kw = reduce(lambda kw, var: kw.replace(f'%{var}', combination[var]), combination, seed)
-        combined.append(new_kw)
+        # Use `reduce` to loop over the keyword several times and replace all variables
+        new_kw = reduce(lambda kw, var: kw.replace(var_char + var, combination[var]), combination, seed)
 
-    return combined
+        yield new_kw
 
 
-def matrix(seeds: list, modifiers: dict):
+def matrix(seeds: list, modifiers: dict, var_char=r'%'):
     """Combine named modifier lists with a list of seed keywords containing variables."""
-    combined_kws = []
     for seed in seeds:
-        mods_subset = _subset_modifiers(seed, modifiers)
-        if len(mods_subset) > 0:
-            combined_kws.extend(_combine(seed, mods_subset))
-        else:
-            combined_kws.append(seed)
+        # Subset modifiers so that we don't pass modifiers that aren't contained in the keyword
+        mods_subset = _subset_modifiers(seed, modifiers, var_char)
 
-    return combined_kws
+        # Only run _combine function if there are variables to replace
+        if len(mods_subset) > 0:
+            for new_kw in _combine(seed, mods_subset, var_char):
+                yield new_kw
+        else:
+            yield seed
 
 
 if __name__ == '__main__':
-    print(matrix(SEEDS, MODIFIERS))
-    # matrix(SEEDS, MODIFIERS)
+    print(list(matrix(SEEDS, MODIFIERS)))
