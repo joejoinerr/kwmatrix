b3b15956ea9e453ed666806c5f99c459d566933f
Joe Joiner <joejoinerr@gmail.com>
2020-12-05 22:34:39 +0000

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+from setuptools import setup
+
+HERE = Path(__file__).parent
+README = (HERE / 'README.md').read_text()
+
+setup(
+    name='kwmatrix',
+    version='1.0.0',
+    description='Combine keywords with several modifiers for SEO',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url='https://github.com/joejoinerr/kwmatrix',
+    author='Joe Joiner',
+    author_email='joe@legato.digital',
+    license='GPL'
+)
